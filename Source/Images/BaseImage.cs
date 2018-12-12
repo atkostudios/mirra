2149ec6ad1c432ab603febeb7dd42ea2175dec5a
comfort-stereo <jakeploskey@gmail.com>
2018-12-12 15:14:26 -0800

    Add attribute methods

--- Source/Images/BaseImage.cs
+++ Source/Images/BaseImage.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using Atko.Mirra.Utility;
@@ -25,7 +27,44 @@ namespace Atko.Mirra.Images
         {
             Member = member;
             ShortName = Member.Name.SubstringAfterLast(".");
-            IsCompilerGenerated = Member.IsDefined(typeof(CompilerGeneratedAttribute));
+            IsCompilerGenerated = HasAttribute<CompilerGeneratedAttribute>();
+        }
+
+        public bool HasAttribute(Type type, bool inherit = true)
+        {
+            return Member.IsDefined(type, inherit);
+        }
+
+        public bool HasAttribute<T>(bool inherit = true) where T : class
+        {
+            return HasAttribute(typeof(T), inherit);
+        }
+
+        [return: AllowNull]
+        public Attribute Attribute(Type type, bool inherit = true)
+        {
+            return Member.GetCustomAttribute(type, inherit);
+        }
+
+        [return: AllowNull]
+        public T Attribute<T>(bool inherit = true) where T : class
+        {
+            return Attribute(typeof(T), inherit) as T;
+        }
+
+        public IEnumerable<Attribute> Attributes(Type type, bool inherit = true)
+        {
+            return CustomAttributeExtensions.GetCustomAttributes(type, inherit);
+        }
+
+        public IEnumerable<Attribute> Attributes(bool inherit = true)
+        {
+            return Attributes(typeof(Attribute), inherit);
+        }
+
+        public IEnumerable<Attribute> Attributes<T>(bool inherit = true) where T : class
+        {
+            return Attributes(typeof(T), inherit);
         }
     }
 }
